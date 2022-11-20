b67ed14f110e8050287443da0f18b0450a5a5745
DawidWebb <tomasz.matras@developerweb.pl>
2022-11-20 20:55:26 +0100

    add service17_22, modify Council compoenent

--- src/templates/Council/Council.js
+++ src/templates/Council/Council.js
@@ -1,26 +1,43 @@
+import styles from "./council.module.scss";
 
-import styles from "./council.module.scss"
-
-const Council = ()=>{
+const Council = () => {
 	return (
 		<div className={styles.wrapper}>
 			<div className={styles.image}></div>
 			<div className={styles.inside}>
 				<h1>Rada Duszpasterska Parafii Mokrzyska</h1>
 				<div className={styles.links}>
-					<a href={"./Zarządzenie_wyborów_Rad_Duszpasterskich_w_Diecezji_Tarnowskiej.pdf"} target="blanc">
+					<a
+						href="./documents/Zarzadzenie_wyborow_Rad_Duszpasterskich_w_Diecezji_Tarnowskiej.pdf"
+						target="blank"
+					>
 						Zarządzenie wyborów Rad Duszpasterskich w Diecezji Tarnowskiej
 					</a>
 					<a
-						href={"./Regulamin_wyboru_Parafialnej_Rady_Duszpasterskiej_w_Dziecezji_Tarnowskiej.pdf"}
-						target="blanc"
+						href="./documents/Regulamin_wyboru_Parafialnej_Rady_Duszpasterskiej_w_Diecezji_Tarnowskiej.pdf"
+						target="blank"
 					>
-						Regulamin wyboru Parafialnej Rady Duszpasterskiej w Dziecezji Tarnowskiej
+						Regulamin wyboru Parafialnej Rady Duszpasterskiej w Diecezji Tarnowskiej
+					</a>
+					<a href="./documents/Wyniki_wyborow_do_Rady_Duszpasterskiej.pdf" target="blank">
+						Wyniki wyborów do Rady Duszpasterskiej
+					</a>
+					<a
+						href="./documents/Statut_Parafialnej_Rady_Duszpasterskiej_w_Diecezji_Tarnowskiej.pdf"
+						target="blank"
+					>
+						Statut Parafialnej rady Duszpasterskiej w Diecezji Tarnowskiej
+					</a>
+					<a
+						href="./documents/Statut_Parafialnej_Rady_Ekonomicznej_w_Diecezji_Tarnowskiej.pdf"
+						target="blank"
+					>
+						Statut Parafialnej Rady Ekonomicznej w Diecezji Tarnowskiej
 					</a>
 				</div>
 			</div>
 		</div>
 	);
-}
+};
 
-export default Council
+export default Council;
--- src/templates/Services2022/index.js
+++ src/templates/Services2022/index.js
@@ -1,6 +1,6 @@
 export { default as Service1 } from "./Service1_Church";
 export { default as Service21_22 } from "./Service21_fatima_2022";
-export { default as Service17_22 } from "./Service17_odpust_2022";
+export { default as Service17_22 } from "./Service17_koncert_2022";
 export { default as Service18_22 } from "./Service18_sandomierz_2022";
 export { default as Service19_22 } from "./Service19_budowa_przedsz_2022";
 export { default as Service20_22 } from "./Service20_Piknik_2022";
