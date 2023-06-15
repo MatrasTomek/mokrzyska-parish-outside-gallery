61beb4965531281f2eaacd728feb152fb946eff7
Tomasz Matras <tomasz.matras@developerweb.pl>
2023-06-15 23:19:48 +0200

    Add news

--- src/templates/StartViev/components/NewsView.js
+++ src/templates/StartViev/components/NewsView.js
@@ -1,28 +1,34 @@
 import React from "react";
-import { Document, Page } from "react-pdf";
 import styles from "./newsView.module.scss";
 
 const NewsView = () => {
   return (
     <div className={styles.newsWrapper}>
       <h2>Aktualności</h2>
+
       <div className={styles.newsItem}>
-        <div className={styles.newsTitle}>
-          {" "}
-          <h5>13.06.2023</h5>
-          <h3>Diecezjalne Studium Rodziny</h3>
-        </div>
-        <div className={`${styles.img} ${styles.img1}`}></div>
+        <a
+          href="./documents/news/Diecezjalne_Studium_Rodziny.png"
+          target="blank"
+        >
+          <div className={styles.newsTitle}>
+            {" "}
+            <h3>Diecezjalne Studium Rodziny</h3>
+          </div>
+          <div className={`${styles.img} ${styles.img1}`}></div>
+        </a>
       </div>
       <div className={styles.newsItem}>
-        <div className={styles.newsTitle}>
-          {" "}
-          <h5>12.06.2023</h5>
-          <h3>Pielgrzymka do Włoch</h3>
-        </div>
-        <div className={styles.pdf}>
-          <Document file="../../../assets/newsItems/Program_pielgrzymki_wlochy.pdf"></Document>
-        </div>
+        <a
+          href="./documents/news/Program_pielgrzymki_IT_2023.pdf"
+          target="blank"
+        >
+          <div className={styles.newsTitle}>
+            {" "}
+            <h3>Pielgrzymka do Włoch</h3>
+          </div>
+          <div className={`${styles.img} ${styles.img2}`}></div>
+        </a>
       </div>
     </div>
   );
