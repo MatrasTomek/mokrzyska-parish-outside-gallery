1d80fbb12175cd2b330fb192dc607b626d2e88c2
Tomasz Matras <tomasz.matras@developerweb.pl>
2023-09-17 10:06:43 +0200

    Add news items

--- src/templates/StartViev/components/NewsView.js
+++ src/templates/StartViev/components/NewsView.js
@@ -12,6 +12,30 @@ const NewsView = () => {
         >
           <div className={styles.newsTitle}>
             {" "}
+            <h3>Diecezjalne spotkanie Nauczycieli i Wychowawców</h3>
+          </div>
+          <div className={`${styles.img} ${styles.img6}`}></div>
+        </a>
+      </div>
+      <div className={styles.newsItem}>
+        <a
+          href="./documents/news/czerny_potok.jpg"
+          target="blank"
+        >
+          <div className={styles.newsTitle}>
+            {" "}
+            <h3>Ku trzeźwości z Kościołem</h3>
+          </div>
+          <div className={`${styles.img} ${styles.img5}`}></div>
+        </a>
+      </div>
+      <div className={styles.newsItem}>
+        <a
+          href="./documents/news/ku_trzezwosci.pdf"
+          target="blank"
+        >
+          <div className={styles.newsTitle}>
+            {" "}
             <h3>Wycieczka „Skarby Podkarpacia”</h3>
           </div>
           <div className={`${styles.img} ${styles.img4}`}></div>
