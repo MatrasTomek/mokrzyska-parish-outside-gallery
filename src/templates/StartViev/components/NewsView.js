e398c00c7a3d796e925110aa7bc9be05e56c5ec1
Tomasz Matras <tomasz.matras@developerweb.pl>
2023-09-21 08:56:50 +0200

    Refresh news

--- src/templates/StartViev/components/NewsView.js
+++ src/templates/StartViev/components/NewsView.js
@@ -7,7 +7,19 @@ const NewsView = () => {
       <h2>Aktualności</h2>
       <div className={styles.newsItem}>
         <a
-          href="./documents/news/skarby_podkarpacia.png"
+          href="./documents/news/Plan_skarby_podkarpacia.pdf"
+          target="blank"
+        >
+          <div className={styles.newsTitle}>
+            {" "}
+            <h3>Plan Wycieczko – Pielgrzymki „Skarby Podkarpacia”</h3>
+          </div>
+          <div className={`${styles.img} ${styles.img3}`}></div>
+        </a>
+      </div>
+      <div className={styles.newsItem}>
+        <a
+          href="./documents/news/czarny_potok.jpg"
           target="blank"
         >
           <div className={styles.newsTitle}>
@@ -19,7 +31,7 @@ const NewsView = () => {
       </div>
       <div className={styles.newsItem}>
         <a
-          href="./documents/news/czerny_potok.jpg"
+          href="./documents/news/ku_trzezwosci.pdf"
           target="blank"
         >
           <div className={styles.newsTitle}>
@@ -39,42 +51,6 @@ const NewsView = () => {
             <h3>Wycieczka „Skarby Podkarpacia”</h3>
           </div>
           <div className={`${styles.img} ${styles.img4}`}></div>
-        </a>
-      </div>
-      <div className={styles.newsItem}>
-        <a
-          href="./documents/news/Rekrutacja.pdf"
-          target="blank"
-        >
-          <div className={styles.newsTitle}>
-            {" "}
-            <h3>Rekrutacja na studia magisterskie z teologii</h3>
-          </div>
-          <div className={`${styles.img} ${styles.img3}`}></div>
-        </a>
-      </div>
-      <div className={styles.newsItem}>
-        <a
-          href="./documents/news/Diecezjalne_Studium_Rodziny.png"
-          target="blank"
-        >
-          <div className={styles.newsTitle}>
-            {" "}
-            <h3>Diecezjalne Studium Rodziny</h3>
-          </div>
-          <div className={`${styles.img} ${styles.img1}`}></div>
-        </a>
-      </div>
-      <div className={styles.newsItem}>
-        <a
-          href="./documents/news/Program_pielgrzymki_IT_2023.pdf"
-          target="blank"
-        >
-          <div className={styles.newsTitle}>
-            {" "}
-            <h3>Pielgrzymka do Włoch</h3>
-          </div>
-          <div className={`${styles.img} ${styles.img2}`}></div>
         </a>
       </div>
     </div>
