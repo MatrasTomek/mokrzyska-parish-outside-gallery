20a989bb12e5a4e043ab3bf19a10e31d0eb85727
Tomasz Matras <tomasz.matras@developerweb.pl>
2023-09-22 09:17:01 +0200

    Update news items

--- src/templates/StartViev/components/NewsView.js
+++ src/templates/StartViev/components/NewsView.js
@@ -12,9 +12,33 @@ const NewsView = () => {
         >
           <div className={styles.newsTitle}>
             {" "}
-            <h3>Plan Wycieczko – Pielgrzymki „Skarby Podkarpacia”</h3>
+            <h3>Budujemu studnie w Czadzie</h3>
+            <p>W dniu dzisiejszym rusza akcja pomocy misjom przez budowanie
+                studni głębinowych, dzięki zbiórce zużytych (dobrych i zepsutych)
+                przedmiotów codziennego użytku. W tym przypadku chodzi o telefony
+                komórkowe (klawiszowe), smartfony, tablety i laptopy.
+                Diecezja tarnowska dzięki jej misjonarzom w Afryce wybudowała już 192
+                studnie głębinowe sfinansowane przez różne podmioty (kolędnicy misyjni,
+                różni ofiarodawcy - w tym księża sercanie, a także Dzieło Misyjne
+                Diecezji Tarnowskiej z funduszu uzyskanego za sprzedaż makulatury i
+                plastikowych nakrętek).
+            </p>
+            <p>W naszej parafii prowadzona również będzie zbiórka do 22 października. Zużyte sprzęty można
+            składać w zakrystii lub na plebanii. Z góry dziękuję za włączenie się w tę akcję pomocy misjonarzom.</p>
           </div>
           <div className={`${styles.img} ${styles.img3}`}></div>
+        </a>
+      </div>
+      <div className={styles.newsItem}>
+        <a
+          href="./documents/news/Plan_skarby_podkarpacia.pdf"
+          target="blank"
+        >
+          <div className={styles.newsTitle}>
+            {" "}
+            <h3>Plan Wycieczko – Pielgrzymki „Skarby Podkarpacia”</h3>
+          </div>
+          <div className={`${styles.img} ${styles.img4}`}></div>
         </a>
       </div>
       <div className={styles.newsItem}>
