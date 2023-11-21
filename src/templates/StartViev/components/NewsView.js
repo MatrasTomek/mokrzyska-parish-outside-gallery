7d022f0e45f145618fa7362e163e8343ac678354
Tomasz Matras <tomasz.matras@developerweb.pl>
2023-11-21 20:53:10 +0100

    add news item

--- src/templates/StartViev/components/NewsView.js
+++ src/templates/StartViev/components/NewsView.js
@@ -7,6 +7,18 @@ const NewsView = () => {
       <h2>Aktualności</h2>
       <div className={styles.newsItem}>
         <a
+          href="./documents/news/11_forum_nauczycieli_wychowawcow.png"
+          target="blank"
+        >
+          <div className={styles.newsTitle}>
+            {" "}
+            <h3>XI FORMU NAUCZYCIELI I WYCHOWAWCÓW - Wychowawcy na współczesne czasy.</h3>
+          </div>
+          <div className={`${styles.img} ${styles.img1}`}></div>
+        </a>
+      </div>
+      <div className={styles.newsItem}>
+        <a
           href="./documents/news/plakat_2023_Bochnia.jpg"
           target="blank"
         >
