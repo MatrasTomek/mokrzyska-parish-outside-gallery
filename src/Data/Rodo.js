d66ec3931108b9be1dede1730f1fa04e2a96f0e3
Tomasz Matras <tmatras@whitelabelcoders.com>
2024-06-04 11:00:01 +0200

    Update RODO policy

--- src/Data/Rodo.js
+++ src/Data/Rodo.js
@@ -8,18 +8,25 @@ export const RODO_AND_COOKIE = [
   },
   {
     id: 1,
+    title: "Wizerunek",
+    content:
+      "Informujemy uczestników, że w trakcie nabożeństw będą przeprowadzone działania mające na celu stworzenie relacji fotograficznej z wydarzenia. Uczestnicy biorąc udział w nabożeństwach udzielają (w rozumieniu przepisów Ustawy z dnia 4 lutego 1994 r. o prawie autorskim i prawach pokrewnych) zezwolenia na rozpowszechnianie swojego wizerunku, utrwalonego za pomocą wszelkich technik fotograficznych i nagraniowych. Zgoda została wyrażona dobrowolnie i w każdym momencie może zostać odwołana. Odwołanie zgody może nastąpić poprzez złożenie pisemnego oświadczenia i wysłania jego skanu na adres:",
+    email: "mokrzyskaparafia@gmial.com",
+  },
+  {
+    id: 2,
     title: null,
     content:
       "Właściciel strony może gromadzić dane przesłane automatycznie przezprzeglądarkę użytkownika przeglądającego stronę (to głównie IP użytkownika, nazwa systemu operacyjnego użytkownika, nazwę i wersję przeglądarki). Dane te gromadzone są głównie w celach statystycznych. Nie zawierają one żadnych danych osobistych użytkownika.",
   },
   {
-    id: 2,
+    id: 3,
     title: "Pliki tymczasowe",
     content:
       "Pliki cookies wykorzystywane są w celu dostosowania zawartości podstron do preferencji użytkownika, optymalizacji korzystania ze strony internetowej oraz w celach statystycznych, aby pomóc nam w dostosowywaniu struktury, funkcjonalności i zawartości serwisu. Są one zapisywane na urządzeniu końcowym użytkownika. Ta strona nie wymaga akceptacji ciasteczek, jednak ich akceptacja pozwoli w pełni korzystać ze wszystkich funkcjonalności serwisu.  W każdej chwili użytkownik może zmienić w swojej przeglądarce ustawienia dotyczące plików cookie, w tym również je zablokować,niemniej w takim przypadku nie gwarantujemy poprawnego działania serwisu. Sposób blokowania lub kasowania tych plików różni się w zależności od używanej przeglądarki internetowej. Więcej informacji o tym ustawieniu można znaleźć na stornie internetowej producenta Twojej przeglądarki. Pamiętaj! Ciasteczka nie są szkodliwe ani dla użytkownika, ani dla jego komputera i danych znajdujących się na nim.",
   },
   {
-    id: 3,
+    id: 4,
     title: "Pozostałe informacje",
     content:
       "Niniejsza polityka prywatności jest dostępna na stronie ... i jest dostępna dla każdego odwiedzającego stronę. Właściciel strony zastrzega sobie prawo do wprowadzania zmian w Polityce Prywatności. Każda zmiana powyższej polityki będzie opublikowana na stronie ... Internauta odwiedzający stronę  www.developerweb.pl, akceptuje powyższą politykę. Użytkownik, który nie akceptuje warunków powyższej polityki zobowiązany jest do powstrzymania się od przeglądania stron serwisu.",
--- src/templates/RodoViev/RodoViev.js
+++ src/templates/RodoViev/RodoViev.js
@@ -15,12 +15,18 @@ const RodoViev = () => {
       <h3>{rodoDate[0].title}</h3>
       <p>{rodoDate[0].content}</p>
       <a href="mailto:mokrzyskaparafia@gmial.com">{rodoDate[0].email}</a>
-      <p>{rodoDate[1].content}</p>
 
-      <h3>{rodoDate[2].title}</h3>
-      <p>{rodoDate[2].content}</p>
+      <h3>{rodoDate[1].title}</h3>
+      <p>{rodoDate[1].content}</p>
+      <a href="mailto:mokrzyskaparafia@gmial.com">{rodoDate[1].email}</a>
+
+
+
       <h3>{rodoDate[3].title}</h3>
       <p>{rodoDate[3].content}</p>
+      <h3>{rodoDate[4].title}</h3>
+      <p>{rodoDate[4].content}</p>
+      <p>{rodoDate[2].content}</p>
     </div>
   );
   return <>{contentViev}</>;
