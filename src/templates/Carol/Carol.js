0b718d351b5c343fae839d61fc6eac7913c42ca8
Tomasz Matras <tmatras@whitelabelcoders.com>
2024-11-23 08:20:50 +0100

    Add content to Carol component

--- src/templates/Carol/Carol.js
+++ src/templates/Carol/Carol.js
@@ -5,8 +5,35 @@ const Carol = () => {
 		<div className={styles.wrapper}>
 			<div className={styles.image}></div>
 			<div className={styles.inside}>
-				<h1>Wizyta Duszpasterska/Kolęda 2024/2025</h1>
-				<p>Ten serwis będzie aktualizowany</p>
+				<h1>WIZYTA DUSZPASTERSKA</h1>
+				<h3>1 grudnia (NIEDZIELA) 2024 roku, ulica Gibałówka.</h3>
+<p><span>Rozpoczęcie o godzinie 16.30.</span> Od Państwa Karoliny i Daniela Borowiec, zakończenie u Państwa Zbigniewa i Edyty Gaczoł.</p>
+
+<h3>1 grudnia (NIEDZIELA) 2024 roku, ulica Wspólna.</h3>
+<p><span>Rozpoczęcie o godzinie 18.30.</span> Od Państwa Tomasza i Magdaleny Grzesik, zakończenie u Państwa Kazimierza i Grażyny Włudyków.</p>
+
+<h3>7 grudnia (SOBOTA) 2024 roku, ulica Parafialna.</h3>
+<p><span>Rozpoczęcie o godzinie 9.00.</span> Od Pana Przemysława Łagosza, zakończenie u Państwa Stanisława i Ewy Bernady, koło sklepu Biedronka.</p>
+
+<h3>14 grudnia (SOBOTA) 2024, ulica Wiślana i Ludowa.</h3>
+<p><span>Rozpoczęcie o godzinie 9.00.</span> Od Pana Władysława Szydłowskiego, zakończenie u Państwa Norberta i Małgorzaty Kubiczek.</p>
+
+<h3>15 grudnia (NIEDZIELA) 2024, ulica Wiślana.</h3>
+<p><span>Rozpoczęcie o godzinie 16.30.</span> Od Państwa Sebastiana i Nikoli Franczyk, zakończenie u Państwa Wojciecha i Wiolety Nowak przy rondzie.</p>
+
+<h3>22 grudnia (NIEDZIELA) 2024, ulica Na Stoku.</h3>
+<p><span>Rozpoczęcie o godzinie 16.30.</span> Od Państwa Artura i Zofii Król, zakończenie u Państwa Maksymiliana i Justyny Chmielarzów.</p>
+
+<h3>28 grudnia (SOBOTA) 2024, ulica Januszowska.</h3>
+<p><span>Rozpoczęcie o godzinie 9.00.</span> Od Państwa Łukasza i Katarzyny Martyków, zakończenie u Państwa Józefa i Doroty Wąsik.</p>
+
+<h3>29 grudnia (NIEDZIELA) 2024, ulica Jeziorańska.</h3>
+<p><span>Rozpoczęcie o godzinie 17.30.</span> Od Państwa Grzegorza i Doroty Kwiatek, zakończenie u Państwa Michała i Karoliny Koczwarów.</p>
+
+<h3>30 grudnia (PONIEDZIAŁEK) 2024, ulica Jana Kantego.</h3>
+<p><span>Rozpoczęcie o godzinie 16.00.</span> Od Państwa Grzegorza i Anny Baniak, zakończenie u Pani Moniki Rudnik.</p>
+
+
 			</div>
 		</div>
 	);
