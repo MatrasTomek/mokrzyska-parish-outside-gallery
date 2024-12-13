e34b325fe528a8078e218f129d660e6217c59fc7
Tomasz Matras <tmatras@whitelabelcoders.com>
2024-12-13 13:38:13 +0100

    Update Carol component

--- src/templates/Carol/Carol.js
+++ src/templates/Carol/Carol.js
@@ -33,6 +33,69 @@ const Carol = () => {
 <h3>30 grudnia (PONIEDZIAŁEK) 2024, ulica Jana Kantego.</h3>
 <p><span>Rozpoczęcie o godzinie 16.00.</span> Od Państwa Grzegorza i Anny Baniak, zakończenie u Pani Moniki Rudnik.</p>
 
+<h3>4 Styczeń (SOBOTA) 2025, ulica Stanisławska.</h3>
+<p><span>Rozpoczęcie o godzinie 9.00.</span> Od Państwa Zenona i Grażyny Kubala, zakończenie u Państwa Stanisława i Barbary Wyczesany.</p>
+
+<h3>5 Styczeń (NIEDZIELA) 2025, ulica Leśny Stok.</h3>
+<p><span>Rozpoczęcie o godzinie 16.30</span> Od Państwa Marka i Marzeny Blum, zakończenie u Państwa Bartłomieja i Anny Tabaszowskich.</p>
+
+<h3>6 Styczeń (PONIEDZIAŁEK) 2025, ulica Osiedle Szkolne.</h3>
+<p><span>Rozpoczęcie o godzinie 16.30</span> Od Państwa Jacka i Jolanty Szczygiełek, zakończenie u Państwa Dawida i Iwony Halik.</p>
+
+<h3>7 Styczeń (WTOREK) 2025, ulica Siostry Katarzyny Przybytniak.</h3>
+<p><span>Rozpoczęcie o godzinie 16.00</span> Od Pani Zofii Wszołek, zakończenie u Pani Haliny Kuciel.</p>
+
+<h3>11 Styczeń (SOBOTA) 2025, ulica Gminna i Potokowa.</h3>
+<p><span>Rozpoczęcie o godzinie 9.00</span> Od Państwa Małgorzaty i Roberta Motak, zakończenie u Państwa Michała i Katarzyny Dziedzic.</p>
+
+<h3>12 Styczeń (NIEDZIELA) 2025, ulica TRAKT KRÓLEWSKI. TYLKO PRAWA STRONA OD SKLEPU CENTRUM</h3>
+<p><span>Rozpoczęcie o godzinie 16.30</span> Od Państwa Dawida i Pauliny Gicala, zakończenie u Państwa Tadeusza i Anny Pasierb.</p>
+
+<h3>13 Styczeń (PONIEDZIAŁEK) 2025, ulica Dobrego Pasterza.</h3>
+<p><span>Rozpoczęcie o godzinie 16.00</span> Od Państwa Grzegorza i Marii Kaczmarzyk, zakończenie u Państwa Marcina i Krystyny Pawlik.</p>
+
+<h3>14 Styczeń (WTOREK) 2025, ulica Dobrego Pasterza</h3>
+<p><span>Rozpoczęcie o godzinie 16.00</span> Od Państwa Artura i Moniki Latocha, zakończenie u Państwa Marka i Joanny Niedźwieckich.</p>
+
+<h3>16 Styczeń (CZWARTEK) 2025, ulica Kościelna tylko prawa strona od wjazdu od ulicy Wiślanej.</h3>
+<p><span>Rozpoczęcie o godzinie 16.00</span> Rozpoczęcie o godzinie 16.00. Od Pani Marii Witkowskiej, zakończenie u Państwa Piotra i Marty Wąsik.</p>
+
+<h3>18 Styczeń (SOBOTA) 2025, ulica Kościelna tylko lewa strona od Przedszkola Parafialnego</h3>
+<p><span>Rozpoczęcie o godzinie 9.00</span> Od Pana Ryszarda Zachary, zakończenie u Państwa Tomasza i Renaty Pasierb.</p>
+
+<h3>19 Styczeń (NIEDZIELA) 2025, ulica Kościelna tylko prawa strona od wjazdu od ulicy Wiślanej.</h3>
+<p><span>Rozpoczęcie o godzinie 16.30</span> Od Pani Danuty Pietras, zakończenie u Państwa Bolesława i Doroty Kazek.</p>
+
+<h3>20 Styczeń (PONIEDZIAŁEK) 2025, ulica Stolarska</h3>
+<p><span>Rozpoczęcie o godzinie 9.00</span> Od Państwa Dariusza i Anny Robak, zakończenie u Państwa Józefa i Katarzyny Gicala.</p>
+
+<h3>21 Styczeń (WTOREK) 2025, ulica Siostry Faustyny od strony Bucza</h3>
+<p><span>Rozpoczęcie o godzinie 9.00</span> Od Państwa Dominika i Alicji Szkarłat, zakończenie u Państwa Damiana i Beaty Kazek.</p>
+
+<h3>23 Styczeń (CZWARTEK) 2025, ulica Siostry Faustyny od strony Bucza</h3>
+<p><span>Rozpoczęcie o godzinie 9.00</span> Od Państwa Sebastiana i Ewy Korygi, zakończenie u Państwa Wiesława i Małgorzaty Bober.</p>
+
+<h3>25 Styczeń (SOBOTA) 2025, ulica Siostry Faustyny</h3>
+<p><span>Rozpoczęcie o godzinie 15.00</span> Od Państwa Leszka i Renaty Pakuła, zakończenie u Państwa Marka i Jadwigi Adamczyk.</p>
+
+<h3>26 Styczeń (NIEDZIELA) 2025, ulica TRAKT KRÓLEWSKI. TYLKO PRAWA STRONA OD SKLEPU CENTRUM</h3>
+<p><span>Rozpoczęcie o godzinie 16.30</span> Od Państwa Stanisława i Ewy Latochów, zakończenie u Państwa Marcina i Moniki Grodny.</p>
+
+<h3>27 Styczeń (PONIEDZIAŁEK) 2025, ulica TRAKT KRÓLEWSKI. TYLKO PRAWA STRONA OD SKLEPU CENTRUM</h3>
+<p><span>Rozpoczęcie o godzinie 9.00</span> Od Państwa Jana i Jadwigi Szkodny, zakończenie u Państwa Tomasza i Dagmary Matras.</p>
+
+<h3>8 Lutego (SOBOTA) 2025, ulica SĄSIEDZKA I ZARZECZ</h3>
+<p><span>Rozpoczęcie o godzinie 9.00</span> Rozpoczęcie o godzinie 9.00. Od Pani Katarzyny Kielan, zakończenie u Państwa Tomasza i Pauliny Biedak.</p>
+
+<h3>23 Styczeń (CZWARTEK) 2025, ulica Siostry Faustyny od strony Bucza</h3>
+<p><span>Rozpoczęcie o godzinie 9.00</span> Od Państwa Sebastiana i Ewy Korygi, zakończenie u Państwa Wiesława i Małgorzaty Bober.</p>
+
+<h3>15 Lutego (SOBOTA) 2025, ulica TRAKT KRÓLEWSKI. TYLKO LEWA STRONA OD SKLEPU CENTRUM</h3>
+<p><span>Rozpoczęcie o godzinie 9.00</span> Rozpoczęcie o godzinie 9.00. Od Pana Stanisława Koczwary, zakończenie u Państwa Daniela i Moniki Klimek.</p>
+
+<h3>22 Lutego (SOBOTA) 2025, ulica TRAKT KRÓLEWSKI ORAZ ULICA WERONIKI PAŁACH. TYLKO LEWA STRONA OD SKLEPU CENTRUM</h3>
+<p><span>Rozpoczęcie o godzinie 9.00</span> Od Państwa Marka i Agnieszki Sobol, zakończenie u Państwa Bolesława i Wilhelminy Bocian.</p>
+
 
 			</div>
 		</div>
