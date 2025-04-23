37642cae47d513ebc291accf41c46ad54aba4f3e
Tomasz Matras <matras.tomasz@outlook.com>
2025-04-23 20:12:25 +0200

    News update

--- src/templates/StartViev/components/NewsView.js
+++ src/templates/StartViev/components/NewsView.js
@@ -1,39 +1,37 @@
-import React from "react";
-import styles from "./newsView.module.scss";
+import React from 'react';
+import styles from './newsView.module.scss';
 
 const NewsView = () => {
-  return (
-    <div className={styles.newsWrapper}>
-      <h2>Aktualności</h2>
-      <div className={styles.newsItem}>
-
-        <a
-          href="./documents/news/3_forum_rad_duspasterskich.png"
-          target="blank"
-        >
-          <div className={styles.newsTitle}>
-            {" "}
-            <h3>Pielgrzymi Nadzieji - III FORUM PARAFIALNYCH RAD DUSZPASTERSKICH</h3>
-          </div>
-
-        </a>
-      </div>
-      <div className={styles.newsItem}>
-        <a
-          href="./documents/news/Stanowczy_sprzeciw_wobec_działań_MEN.pdf"
-          target="blank"
-        >
-          <div className={styles.newsTitle}>
-            {" "}
-            <h3>„Stanowczy sprzeciw wobec działań MEN”
-Oświadczenie Dyrektorów Wydziałów Katechetycznych</h3>
-          </div>
-
-        </a>
-      </div>
-
-    </div>
-  );
+	return (
+		<div className={styles.newsWrapper}>
+			<h2>Aktualności</h2>
+			<div className={styles.newsItem}>
+				<a href="./documents/news/Komunikat_Biskupa_Tarnowskiego.pdf" target="blank">
+					<div className={styles.newsTitle}>
+						{' '}
+						<h3>Komunikat Biskupa Tarnowskiego w związku ze śmiercią papieża Franciszka</h3>
+					</div>
+				</a>
+			</div>
+			,
+			<div className={styles.newsItem}>
+				<a href="./documents/news/3_forum_rad_duspasterskich.png" target="blank">
+					<div className={styles.newsTitle}>
+						{' '}
+						<h3>Pielgrzymi Nadzieji - III FORUM PARAFIALNYCH RAD DUSZPASTERSKICH</h3>
+					</div>
+				</a>
+			</div>
+			<div className={styles.newsItem}>
+				<a href="./documents/news/Stanowczy_sprzeciw_wobec_działań_MEN.pdf" target="blank">
+					<div className={styles.newsTitle}>
+						{' '}
+						<h3>„Stanowczy sprzeciw wobec działań MEN” Oświadczenie Dyrektorów Wydziałów Katechetycznych</h3>
+					</div>
+				</a>
+			</div>
+		</div>
+	);
 };
 
 export default NewsView;
