3582ba0c47a969d55cec8d9c8aa0c0adf04afe81
Tomasz Matras <matras.tomasz@outlook.com>
2025-05-09 22:14:03 +0200

    Request update

--- src/helpers/request.js
+++ src/helpers/request.js
@@ -1,11 +1,8 @@
-import axios from "axios";
+import axios from 'axios';
 
 const request = axios.create({
-  // baseURL: "https://backend-for-parafia-mokrzyska.herokuapp.com/",
-  // baseURL: "https://backend-parish-clone.herokuapp.com/",
-  // baseURL: "http://localhost:8000",
-  baseURL: "https://parafiaapp.b4a.app",
-  validateStatus: false,
+	baseURL: 'https://www.tslapp.ovh/parish',
+	validateStatus: false,
 });
 
 export default request;
