471aa43d8dab0498546abeccb170dc894424f68c
Tomasz Matras <matras.tomasz@outlook.com>
2025-06-13 07:39:46 +0200

    News update

--- src/templates/StartViev/components/NewsView.js
+++ src/templates/StartViev/components/NewsView.js
@@ -6,6 +6,14 @@ const NewsView = () => {
 		<div className={styles.newsWrapper}>
 			<h2>Aktualności</h2>
 			<div className={styles.newsItem}>
+				<a href="./documents/news/Diecezjalne-Swieto-Rodziny-2025.jpg" target="blank">
+					<div className={styles.newsTitle}>
+						{' '}
+						<h3>Diecezjalne Święto Rodziny - 15.06.25r. - Stary Sącz</h3>
+					</div>
+				</a>
+			</div>
+			<div className={styles.newsItem}>
 				<a href="./documents/news/Misje_Swiete.pdf" target="blank">
 					<div className={styles.newsTitle}>
 						{' '}
