2bd521c5736586c50e79b12c79a063538870bc7b
Tomek Matras <matras.tomasz@outlook.com>
2025-11-24 15:21:13 +0100

    Refactor NewsView component, update styles, and add new news image

--- src/templates/StartViev/components/NewsView.js
+++ src/templates/StartViev/components/NewsView.js
@@ -9,12 +9,13 @@ const NewsView = () => {
 				<a href="./documents/news/1000039977.jpg" target="blank">
 					<div className={styles.newsTitle}>
 						{' '}
-						<h3>
+						{/* <h3>
 							Z głębokim wzruszeniem, pragniemy poinformować, że w dniu 16 listopada 2025 roku w domu Księży Emerytów im. św.
 							Józefa w Tarnowie zmarł ks. Marian Wal, pierwszy proboszcz naszej parafii pw. Najświętszego Serca Pana Jezusa w
 							Mokrzyskach, którego duszę otaczamy naszą serdeczną modlitwą.
-						</h3>
+						</h3> */}
 					</div>
+					<div className={`${styles.img} ${styles.img1}`}></div>
 				</a>
 			</div>
 			<div className={styles.newsItem}>
