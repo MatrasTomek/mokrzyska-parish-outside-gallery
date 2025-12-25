7bb564651050cfab14043702d908845b841f6084
Tomek Matras <matras.tomasz@outlook.com>
2025-12-25 16:14:08 +0100

    Update BoxInformation component content, adjust styles, and replace background image; remove unused images

--- src/components/BoxInformation/BoxInformation.js
+++ src/components/BoxInformation/BoxInformation.js
@@ -26,20 +26,15 @@ const BoxInformation = () => {
 		>
 			<div className={styles.inside}>
 				<div className={styles.info}>
-					{/* <p className={styles.sentence}
-
-     >„Niech Duch Świętej Rodziny z Nazaretu
-panuje we wszystkich chrześcijańskich domach”
-św. Jan Paweł II</p>
-<p>W tajemnicy Narodzenia Syna Bożego kieruję najlepsze życzenia do Moich Parafian i Ludzi otwartego serca.</p>
-<p>Niech to wydarzenie, które jest wołaniem Boga o miłość między ludźmi, o zrozumienie, wybaczenie i zgodę, o pokój i przyjaźń w Chrystusie – przemieni nasze serca.</p>
-<p>Niech Boże Dziecię będzie źródłem wszelkich darów, niech błogosławi i opromieni swą łaską wszystkie dni Nowego Roku 2025!.</p>
-
-  <p className={styles.sign1}>Szczęść Boże!</p>
-<p className={styles.sign1}>
-Z darem modlitwy
-Ks. Stanisław Tokarski - Proboszcz
-  </p> */}
+					<p className={styles.sentence}>A Słowo Ciałem się stało i zamieszkało wśród nas.</p>
+					<p>
+						Wszystkim, którzy łączą się w tym czasie w przestrzeni wiary, życzę radości z faktu że, Bóg stał się człowiekiem dla
+						naszego zbawienia i narodził się w Betlejem. Niech ta prawda przenika nasze serca każdego dnia, niech staje się
+						źródłem niewyczerpanej nadziei, radości i pokoju. Niech tęsknota za pełnią życia bez końca, prowadzi nas ku Temu,
+						który Jako jedyny może wypełnić najgłębsze pragnienia ludzkiego serca.{' '}
+					</p>
+					<p className={styles.sign1}>Szczęść Boże.</p>
+					<p className={styles.sign1}>ks. Tomasz Szewczyk</p>
 
 					<div className={styles.exit}>
 						{' '}
--- src/templates/StartViev/components/NewsView.js
+++ src/templates/StartViev/components/NewsView.js
@@ -5,19 +5,19 @@ const NewsView = () => {
 	return (
 		<div className={styles.newsWrapper}>
 			<h2>Aktualności</h2>
-			<div className={styles.newsItem}>
+			{/* <div className={styles.newsItem}>
 				<a href="./documents/news/1000039977.jpg" target="blank">
 					<div className={styles.newsTitle}>
 						{' '}
-						{/* <h3>
+						<h3>
 							Z głębokim wzruszeniem, pragniemy poinformować, że w dniu 16 listopada 2025 roku w domu Księży Emerytów im. św.
 							Józefa w Tarnowie zmarł ks. Marian Wal, pierwszy proboszcz naszej parafii pw. Najświętszego Serca Pana Jezusa w
 							Mokrzyskach, którego duszę otaczamy naszą serdeczną modlitwą.
-						</h3> */}
+						</h3>
 					</div>
 					<div className={`${styles.img} ${styles.img1}`}></div>
 				</a>
-			</div>
+			</div> */}
 			<div className={styles.newsItem}>
 				<a href="./documents/news/Diecezjalne-Swieto-Rodziny-2025.jpg" target="blank">
 					<div className={styles.newsTitle}>
